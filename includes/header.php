af77b47d317da5e10b39f20d50c95692f919e8bf
andmydignity <ahmetaslan139@outlook.com.tr>
2026-04-21 14:39:31 +0300

    Ana temel atıldı

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<div>
+  <img src="" alt="Logo">
+  <a href="index.php">Ana Sayfa</a>
+  <a> Şehir</a>
+  <a> İletişim </a>
+  <a>Özgeçmiş</a>
+  <a> Giriş</a>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Giriş Sayfası</title>
+  </head>
+  <body>
+  <?php include "includes/header.php";?>
+
+
+  
+  <?php include "includes/footer.php";?>
+  </body>
+</html>
